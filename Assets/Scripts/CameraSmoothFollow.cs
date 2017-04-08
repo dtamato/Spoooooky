77df9d42511819781b4ee43cd7118fcbe237c560
Dave Tamayo <djbtamayo@gmail.com>
2017-04-07 22:22:54 -0400

    - Initial push of Unity project - Started working on player movement and invisibility

---
+++ Assets/Scripts/CameraSmoothFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class CameraSmoothFollow : MonoBehaviour {
+
+	[SerializeField] Transform cameraTarget;
+	[SerializeField, Range(0f, 1f)] float cameraDelay = 0.3f;
+
+	Vector3 cameraVelocity = Vector3.zero;
+
+	void FixedUpdate () {
+
+		if (cameraTarget)
+		{
+			Vector3 trackingTarget = new Vector3(cameraTarget.transform.position.x, cameraTarget.transform.position.y, this.transform.position.z);
+
+			this.transform.position = Vector3.SmoothDamp(this.transform.position, trackingTarget, ref cameraVelocity, cameraDelay);		
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+[DisallowMultipleComponent]
+public class PlayerMovement : MonoBehaviour {
+
+	[Header("Movement")]
+	[SerializeField] float movementSpeed = 1;
+
+	[Header("Invisibility")]
+	[SerializeField] float maxInvisibilityTime = 1;
+	[SerializeField] float invisibilityThreshold = 0.5f;
+	[SerializeField] float minInvisibility = 0.2f;
+	float invisibility = 1;
+	float invisibilityTimer = 0;
+	bool canGoInvisible = true;
+
+	[Header("References")]
+	Player rewiredPlayer;
+	SpriteRenderer spriteRenderer;
+	Rigidbody2D rb2d;
+
+	void Awake () {
+
+		rewiredPlayer = ReInput.players.GetPlayer(0);
+		spriteRenderer = this.GetComponentInChildren<SpriteRenderer>();
+		rb2d = this.GetComponentInChildren<Rigidbody2D>();
+	}
+
+	void Update () {
+		
+		UpdateVariables();
+		ProcessMovementInput();
+		ProcessActions();
+		HandleInvisibility();
+	}
+
+	void UpdateVariables () {
+
+		invisibility = spriteRenderer.color.a;
+	}
+
+	void ProcessMovementInput () {
+
+		float horizontalInput = rewiredPlayer.GetAxis("Horizontal");
+		float verticalInput = rewiredPlayer.GetAxis("Vertical");
+		Vector3 movementVector = new Vector3(horizontalInput, verticalInput, 0);
+
+		if(horizontalInput < 0 && spriteRenderer.flipX == true) {
+
+			spriteRenderer.flipX = false;
+		}
+		else if(horizontalInput > 0 && spriteRenderer.flipX == false) {
+
+			spriteRenderer.flipX = true;
+		}
+
+		if(movementVector != Vector3.zero) {
+
+			rb2d.MovePosition(rb2d.transform.position + movementSpeed * movementVector);
+		}
+	}
+
+	void ProcessActions () {
+
+		if(rewiredPlayer.GetButton("Invisibility") && canGoInvisible) {
+
+			IncreaseInvisibility();
+		}
+		else {
+
+			DecreaseInvisibility();
+		}
+	}
+
+	void IncreaseInvisibility () {
+
+		invisibility -= Time.deltaTime;
+		UpdateSpriteInvisibility();
+	}
+
+	void DecreaseInvisibility () {
+
+		invisibility += Time.deltaTime;
+		UpdateSpriteInvisibility();
+	}
+
+	void UpdateSpriteInvisibility () {
+
+		invisibility = Mathf.Clamp(invisibility, minInvisibility, 1);
+		spriteRenderer.color = new Color(1, 1, 1, invisibility);
+	}
+
+	void HandleInvisibility () {
+
+		if(invisibility <= minInvisibility) {
+
+			invisibilityTimer += Time.deltaTime;
+			canGoInvisible = (invisibilityTimer >= maxInvisibilityTime) ? false : true;
+			// Update UI Bar?
+		}
+	}
+}
